b5e993472abd112c64263dad69680fd65eb63fa6
asdfghjkl43m <59603278+asdfghjkl43m@users.noreply.github.com>
2020-01-14 16:49:29 +0700

    dsad

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 <img src = "google.jpg">
 <br>
 <a href="https://www.facebook.com/">my facebook link</a> 
+<a href="profile.php/">My profile link</a>
 </body>
 </html>
---
+++ profile.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>PHP Test </title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+<img src = "image.jpg">
+<br>
+<?php echo '<p>Name:Chu van nam</p>'; ?>
+</body>
+</html>
